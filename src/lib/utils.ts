8751a36fef949fc092ac9f57639331686b6d243c
Sila Mutungi <silamutungi@users.noreply.github.com>
2026-03-31 18:53:37 -0700

    Add src/lib/utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,14 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatCurrency(amount: number): string {
+  return new Intl.NumberFormat(undefined, { style: 'currency', currency: 'USD', maximumFractionDigits: 0 }).format(amount)
+}
+
+export function formatDate(date: string | Date): string {
+  return new Intl.DateTimeFormat(undefined, { dateStyle: 'medium' }).format(new Date(date))
+}
